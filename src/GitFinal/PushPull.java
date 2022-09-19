937fbe9c7e5d275c84275539b54bb2a8a3ce4e49
dahuleashwini <dahuleashwini.34@gmail.com>
2022-09-19 08:18:40 +0530

    First commit

---
+++ src/GitFinal/PushPull.java
@@ -0,0 +1,12 @@
+package GitFinal;
+
+public class PushPull {
+
+	public static void main(String[] args) {
+		System.out.println("Push first");
+		System.out.println("Pull last");
+		System.out.println("used trick and show push and pull concept");
+
+	}
+
+}
