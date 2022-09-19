5175dd8ad58d54f572d61c71cc5dbdb4d954ee16
dahuleashwini <111557187+dahuleashwini@users.noreply.github.com>
2022-09-19 08:36:25 +0530

    Update PushPull.java

--- src/GitFinal/PushPull.java
+++ src/GitFinal/PushPull.java
@@ -4,8 +4,9 @@ public class PushPull {
 
 	public static void main(String[] args) {
 		System.out.println("Push first");
-		System.out.println("Pull last");
+		System.out.println("Pull Last");
 		System.out.println("used trick and show push and pull concept");
+		System.out.println("Push Pull Successful");
 
 	}
 
